a79c485ece2ce9896fd81b2b2ba8815ec08d5bf9
monxy747 <12smnms20@gmail.com>
2025-02-04 23:54:18 +0900

    mainメソッドにforとifのサンプルコードを追加

--- main.go
+++ main.go
@@ -7,4 +7,12 @@ import (
 func main() {
 	fmt.Println("Hello World!!")
 	fmt.Print("HELLO WORLD")
+
+	for i := 0; i < 5; i++ {
+		if i%2 == 0 {
+			fmt.Printf("%d は偶数です\n", i)
+		} else {
+			fmt.Printf("%d は奇数です\n", i)
+		}
+	}
 }
